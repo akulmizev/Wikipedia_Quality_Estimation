e56533f075ed93f26d4529ae84c1c282c86f2d51
Kushal <ktatariya98@gmail.com>
2024-01-09 11:25:04 +0100

    changing ppl code

--- partition.py
+++ partition.py
@@ -11,6 +11,7 @@ import transformers
 import evaluate
 import torch
 import spacy
+from tqdm import tqdm
 
 
 
@@ -78,24 +79,68 @@ class Partition():
 
         return high_quality, low_quality
 
+    # def perplexity(self):
+    #     from spacy.lang.hi import Hindi
+    #     nlp = Hindi()
+    #     nlp.add_pipe('sentencizer')
+    #     perplexity = evaluate.load("perplexity", module_type = 'metric')
+    #     doc = nlp(self.articles[3])
+    #     sents = list(doc.sents)
+    #     predictions = []
+    #     for sent in sents:
+    #         predictions.append(sent.text.strip())
+    #     print(predictions)
+    #     results = perplexity.compute(
+    #                                  model_id='xlm-roberta-base',
+    #                                  # model_id='Davlan/afro-xlmr-base',
+    #                                  add_start_token=False,
+    #                                  predictions=predictions)
+    #     print(list(results.keys()))
+    #     print(results['mean_perplexity'])
+
     def perplexity(self):
         from spacy.lang.hi import Hindi
         nlp = Hindi()
         nlp.add_pipe('sentencizer')
-        perplexity = evaluate.load("perplexity", module_type = 'metric')
         doc = nlp(self.articles[3])
         sents = list(doc.sents)
-        predictions = []
-        for sent in sents:
-            predictions.append(sent.text.strip())
-        print(predictions)
-        results = perplexity.compute(
-                                     model_id='xlm-roberta-base',
-                                     # model_id='Davlan/afro-xlmr-base',
-                                     add_start_token=False,
-                                     predictions=predictions)
-        print(list(results.keys()))
-        print(results['mean_perplexity'])
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model_id = 'xlm-roberta-base'
+        model = transformers.AutoModelForCausalLM.from_pretrained(model_id).to(device)
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+        encodings = tokenizer('\n\n'.join([sent.text.strip() for sent in sents]), return_tensors='pt')
+
+        max_length = model.config.n_positions
+        stride = 512
+        seq_len = encodings.input_ids.size(1)
+
+        nlls = []
+        prev_end_loc = 0
+        for begin_loc in tqdm(range(0, seq_len, stride)):
+            end_loc = min(begin_loc + max_length, seq_len)
+            trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+            input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+            target_ids = input_ids.clone()
+            target_ids[:, :-trg_len] = -100
+
+            with torch.no_grad():
+                outputs = model(input_ids, labels=target_ids)
+
+                # loss is calculated using CrossEntropyLoss which averages over valid labels
+                # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+                # to the left by 1.
+                neg_log_likelihood = outputs.loss
+
+            nlls.append(neg_log_likelihood)
+
+            prev_end_loc = end_loc
+            if end_loc == seq_len:
+                break
+        ppl = torch.exp(torch.stack(nlls).mean())
+        print(ppl)
+
+
 
 
 def main():
