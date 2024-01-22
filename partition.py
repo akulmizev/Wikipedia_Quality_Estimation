d3c6aa58337e15ee2a5e1aafcfcbaa8a79269a67
Kushal <ktatariya98@gmail.com>
2024-01-22 17:46:58 +0100

    incorporating datasets

--- partition.py
+++ partition.py
@@ -120,7 +120,7 @@ class Partition():
         overall_perplexity = []
         # error_arts = []
         for example in tqdm(self.dataset):
-            tokenize_input = tokenizer.tokenize(example['text'], max_length=512, batched=True)
+            tokenize_input = tokenizer.tokenize(example['text'])
             tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)]).cuda()
             with torch.no_grad():
                 loss = model(tensor_input, labels=tensor_input)[0]
