ee4c0b36a902803c4b8d320cfdd2594ba734a068
Kushal <ktatariya98@gmail.com>
2024-03-12 19:13:58 +0100

    changes

--- run.sh
+++ run.sh
@@ -2,5 +2,13 @@
 
 source activate hf
 
+config_file="./config/as/config.json"
+tokenizer_file="./tokenizers/wiki.as.json"
+
 python run_mlm.py \
-  --model
+  --model_type roberta \
+  --config_name $config_file \
+  --tokenizer_name $tokenizer_file \
+  --dataset_name 'WikiQuality/as.filtered' \
+  --validation_split_percentage 5 \
+
