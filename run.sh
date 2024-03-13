95a1d1c87c817df997b3c68a4a61ff545038a363
Kushal <ktatariya98@gmail.com>
2024-03-13 15:49:05 +0100

    changes

--- run.sh
+++ run.sh
@@ -14,9 +14,16 @@ python run_mlm.py \
   --output_dir 'as_filtered' \
   --do_train \
   --do_eval \
-  --per_device_train_batch_size 8 \
+  --per_device_train_batch_size 256 \
   --per_device_eval_batch_size 8 \
   --overwrite_output_dir \
   --overwrite_cache \
   --pad_to_max_length \
-  --line_by_line
+  --line_by_line \
+  --report_to 'wandb' \
+  --run_name 'as_filtered' \
+  --num_train_epochs 5
+#  --push_to_hub \
+#  --hub_model_id 'tiny_bert_as_filtered' \
+#  --hub_private_repo \
+#  --hub_token $hub_token
