40fe9c2f0e6c59a3e9487dce49ed22371bc9a54b
Kushal <ktatariya98@gmail.com>
2024-03-13 14:20:16 +0100

    changes

--- create_model_config.py
+++ create_model_config.py
@@ -8,25 +8,24 @@ vocab_mapper = json.load(open(f"./data/predicted_vocab.json", "r"))
 vocab_size = vocab_mapper[lang]
 
 config = RobertaConfig()
-# config.vocab_size = vocab_size
-# config.num_hidden_layers = 3
-# config.num_attention_heads = 5
-# config.hidden_size = 100
-# config.intermediate_size = 400
-# config.eos_token_id = 2
-# config.pad_token_id = 0
-# config.bos_token_id = 1
-# config.unk_id = 6
-# config.mask_token_id = 3
-# config.cls_token_id = 5
-# config.sep_token_id = 4
-# config.position_id = 512
+config.vocab_size = vocab_size
+config.num_hidden_layers = 3
+config.num_attention_heads = 5
+config.hidden_size = 100
+config.intermediate_size = 400
+config.eos_token_id = 2
+config.pad_token_id = 0
+config.bos_token_id = 1
+config.unk_id = 6
+config.mask_token_id = 3
+config.cls_token_id = 5
+config.sep_token_id = 4
+config.tokenizer_class = "tokenizers.Unigram"
 
-print(config.model_max_length)
 #
-# if not os.path.exists(f"./config"):
-#     os.makedirs(f"./config")
-# config.save_pretrained(f"./config/{lang}")
+if not os.path.exists(f"./config"):
+    os.makedirs(f"./config")
+config.save_pretrained(f"./config/{lang}")
 
 # config = AutoConfig.from_pretrained(f"./config/{lang}")
 # print(config)
