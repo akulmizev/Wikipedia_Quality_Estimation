0027bb093bc4fb3058db55a86d421950af856093
Kushal <ktatariya98@gmail.com>
2024-03-14 19:29:14 +0100

    changes

--- create_text_file.py
+++ create_text_file.py
@@ -1,5 +1,6 @@
 import datasets
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--lang", default=None, type=str, required=True,
@@ -12,6 +13,9 @@ text = []
 dataset = datasets.load_dataset(f"WikiQuality/{lang}.filtered")['train']
 for data in dataset:
     text.append(data['text'])
+
+if not os.path.exists(f"./data/{lang}"):
+    os.makedirs(f"./data/{lang}")
 
 with open(f"./data/{lang}/wiki_{lang}.txt", "w") as f:
     f.write("\n".join(text))
