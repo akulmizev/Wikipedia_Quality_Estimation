f815bffde1c98f5eb9a735d20ba75ce508f91272
Kushal <ktatariya98@gmail.com>
2024-03-25 10:49:21 +0100

    stupid filters

--- create_model_config.py
+++ create_model_config.py
@@ -3,47 +3,16 @@ import json
 import os
 import argparse
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--lang", default=None, type=str, required=True,
-#                     help="specify the language for which the model configuration is to be created. Needs to be in ISO-2 format")
-# parser.add_argument("--output_dir", default="./config", type=str, required=False)
-# parser.add_argument('--base_model', default="roberta", type=str, required=False)
+parser = argparse.ArgumentParser()
+parser.add_argument("--lang", default=None, type=str, required=True,
+                    help="specify the language for which the model configuration is to be created. Needs to be in ISO-2 format")
 
 
+args = parser.parse_args()
 
-lang = "as"
+lang = args.lang
 vocab_mapper = json.load(open(f"./data/predicted_vocab.json", "r"))
 vocab_size = vocab_mapper[lang]
-
-# config = RobertaConfig()
-# config.base_model = "roberta"
-# config.vocab_size = vocab_size
-# config.num_hidden_layers = 3
-# config.num_attention_heads = 5
-# config.hidden_size = 100
-# config.intermediate_size = 400
-# config.pad_token_id = -1
-# config.tokenizer_class = "PreTrainedTokenizerFast"
-# config.max_position_embeddings = 512
-
-# config = BertConfig()
-# config.base_model = "bert"
-# config.vocab_size = vocab_size
-# config.num_hidden_layers = 4
-# config.hidden_size = 312
-# config.intermediate_size = 1200
-# config.num_attention_heads = 12
-# config.max_position_embeddings = 512
-
-# config = RobertaConfig()
-# config.base_model = "roberta"
-# config.vocab_size = vocab_size
-# config.num_hidden_layers = 4
-# config.hidden_size = 312
-# config.intermediate_size = 1200
-# config.num_attention_heads = 12
-# config.max_position_embeddings = 512
-# config.pad_token_id = -1
 
 config = DebertaConfig()
 config.base_model = "deberta"
@@ -55,16 +24,6 @@ config.num_attention_heads = 12
 config.max_position_embeddings = 512
 
 
-
-
-
-#
 if not os.path.exists(f"./config"):
     os.makedirs(f"./config")
 config.save_pretrained(f"./config/{lang}/config_tiny_deberta")
-
-# config = AutoConfig.from_pretrained(f"./config/{lang}")
-# print(config)
-
-# model = AutoModelForMaskedLM.from_config(config=config)
-# print(model.get_input_embeddings().weight.shape[0])
--- create_text_file.py
+++ create_text_file.py
@@ -5,12 +5,15 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("--lang", default=None, type=str, required=True,
                     help="specify the language for which the text file needs to be created. Needs to be in ISO-2 format")
+parser.add_argument("--partition", default=None, type=str, required=True,
+                    help="Which data partition to use")
 
 args = parser.parse_args()
 lang = args.lang
+partition = args.partition
 
 text = []
-dataset = datasets.load_dataset(f"WikiQuality/{lang}.filtered")['train']
+dataset = datasets.load_dataset(f"WikiQuality/{lang}.{partition}")['train']
 for data in dataset:
     text.append(data['text'])
 
